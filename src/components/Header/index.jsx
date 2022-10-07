e4da544a1fe3b8052aa89dcb0f4ed772064aa6a5
Rohith R <rohith.r@sasken.com>
2022-10-07 23:06:51 +0530

    added app title

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,7 +13,7 @@ const Header = ({ setCooridantes, geoCoderRef }) => {
         <AppBar position='static'>
             <Toolbar className={classes.toolbar}>
                 <Typography variant='h5' className={classes.title}>
-                    Travel Advisor
+                    Travel Assist
                 </Typography>
                 <Box style={{ display: 'flex' }}>
                     <Typography variant='h6' className={classes.title}>
